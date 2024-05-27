202ec40e15caedddc4db0d1ae4317cd3081d8cc0
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:13:32 +0700

    Add passwordEncoder in WebSecurityConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springdocjwt/config/WebSecurityConfig.java
+++ src/main/java/id/my/hendisantika/springdocjwt/config/WebSecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -76,4 +78,10 @@ public class WebSecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
