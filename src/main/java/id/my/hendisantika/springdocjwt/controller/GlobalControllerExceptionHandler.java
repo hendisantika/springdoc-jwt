62a84538e4def9a4dccc27c031bbb97c13190f13
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:17:23 +0700

    Add GlobalControllerExceptionHandler🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springdocjwt/controller/GlobalControllerExceptionHandler.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.springdocjwt.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springdoc-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/28/24
+ * Time: 06:17
+ * To change this template use File | Settings | File Templates.
+ */
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleConversion(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
