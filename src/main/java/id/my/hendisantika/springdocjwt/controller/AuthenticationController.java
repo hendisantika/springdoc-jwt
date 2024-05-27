3609af265c28672ead20a37d1b415c33261beded
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:16:51 +0700

    Add authenticate in AuthenticationController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springdocjwt/controller/AuthenticationController.java
+++ src/main/java/id/my/hendisantika/springdocjwt/controller/AuthenticationController.java
@@ -1,7 +1,18 @@
 package id.my.hendisantika.springdocjwt.controller;
 
+import id.my.hendisantika.springdocjwt.model.AuthenticationRequest;
+import id.my.hendisantika.springdocjwt.model.AuthenticationResponse;
+import id.my.hendisantika.springdocjwt.model.Book;
 import id.my.hendisantika.springdocjwt.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +32,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+
+    @Operation(summary = "Authenticate to get access JWT token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success Authentication",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))}),
+            @ApiResponse(responseCode = "400", description = "Failed Authentication",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Failed Authentication",
+                    content = @Content)})
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
 }
