d380132241bef5c5e632ddd5f38f8e5d87fc017f
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:07:13 +0700

    Add doFilterInternal in JwtAuthenticationFilter 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springdocjwt/security/JwtAuthenticationFilter.java
+++ src/main/java/id/my/hendisantika/springdocjwt/security/JwtAuthenticationFilter.java
@@ -1,9 +1,20 @@
 package id.my.hendisantika.springdocjwt.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +33,46 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private JwtService jwtService;
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        boolean isInvalidToken = false;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            isInvalidToken = true;
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+        if (userEmail != null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        } else {
+            isInvalidToken = true;
+        }
+
+        if (isInvalidToken) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "The token is not valid.");
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
