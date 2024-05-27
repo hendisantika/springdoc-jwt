6b356764caf8b6d82d1916ea4d65318909f20877
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:13:21 +0700

    Add authenticationManager in WebSecurityConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springdocjwt/config/WebSecurityConfig.java
+++ src/main/java/id/my/hendisantika/springdocjwt/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import id.my.hendisantika.springdocjwt.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -68,5 +70,10 @@ public class WebSecurityConfig {
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
