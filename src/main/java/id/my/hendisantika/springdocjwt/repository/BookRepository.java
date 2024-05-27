1a02cb3bae4e2066badc59e03f1323d68c9e5083
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:06:16 +0700

    Add BookRepository 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springdocjwt/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.springdocjwt.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springdoc-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/28/24
+ * Time: 06:05
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public class BookRepository {
+}
