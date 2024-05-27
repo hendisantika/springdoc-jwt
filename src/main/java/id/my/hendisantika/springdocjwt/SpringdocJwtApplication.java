0eb7a539977cb5f990e6b6ff8c65fcf4bc16deeb
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:01:13 +0700

    Add required libs 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springdocjwt/SpringdocJwtApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springdocjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springdocjwt/SpringdocJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springdocjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
