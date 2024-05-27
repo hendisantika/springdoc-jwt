35f03cf378cf115c242cdb7a34da6078e71b5659
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:06:07 +0700

    Add AuthenticationResponse 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springdocjwt/model/AuthenticationResponse.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.springdocjwt.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springdoc-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/28/24
+ * Time: 06:04
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class AuthenticationResponse {
+    private String token;
+}
