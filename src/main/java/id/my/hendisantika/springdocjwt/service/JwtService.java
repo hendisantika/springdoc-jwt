bb6473956362be0bec7cfaed7fc44bd2a2dc0a09
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:08:34 +0700

    Add JwtService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springdocjwt/service/JwtService.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.springdocjwt.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springdoc-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/28/24
+ * Time: 06:08
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+}
