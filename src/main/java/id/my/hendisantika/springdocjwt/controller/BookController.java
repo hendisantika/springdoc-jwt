ee837c9d04c558aea88e15de6016fcc03af5b7ae
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:18:21 +0700

    Add findById in BookController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springdocjwt/controller/BookController.java
+++ src/main/java/id/my/hendisantika/springdocjwt/controller/BookController.java
@@ -1,7 +1,15 @@
 package id.my.hendisantika.springdocjwt.controller;
 
+import id.my.hendisantika.springdocjwt.model.Book;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +28,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Book", description = "The Book API. Contains all the operations that can be performed on a book.")
 @RequestMapping("/api/book")
 public class BookController {
+    @Operation(summary = "Get a book by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content)})
+    @GetMapping("/{id}")
+    public Book findById(@PathVariable long id) {
+        Book book1 = new Book();
+        book1.setAuthor("test");
+        book1.setBookName("testBook");
+        return book1;
+    }
+
 }
