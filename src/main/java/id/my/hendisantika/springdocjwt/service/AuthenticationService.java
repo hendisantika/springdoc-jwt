11f1b172cfc38dae136ebaf0f5b496c3aa15f4fd
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:08:11 +0700

    Add authenticate in AuthenticationService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springdocjwt/service/AuthenticationService.java
+++ src/main/java/id/my/hendisantika/springdocjwt/service/AuthenticationService.java
@@ -1,7 +1,10 @@
 package id.my.hendisantika.springdocjwt.service;
 
+import id.my.hendisantika.springdocjwt.model.AuthenticationRequest;
+import id.my.hendisantika.springdocjwt.model.AuthenticationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -24,4 +27,19 @@ public class AuthenticationService {
     private AuthenticationManager authenticationManager;
     @Autowired
     private UserDetailsService userDetailsService;
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userDetailsService.loadUserByUsername(request.getEmail());
+        var jwtToken = jwtService.generateToken(user);
+        var response = new AuthenticationResponse();
+        response.setToken(jwtToken);
+        return response;
+    }
 }
