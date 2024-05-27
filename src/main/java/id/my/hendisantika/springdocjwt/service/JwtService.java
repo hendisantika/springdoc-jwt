3614fc05380de7515723b412bd19e4aa157e7ac5
hendisantika <hendisantika@yahoo.co.id>
2024-05-28 06:08:45 +0700

    Add extractUsername in JwtService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springdocjwt/service/JwtService.java
+++ src/main/java/id/my/hendisantika/springdocjwt/service/JwtService.java
@@ -16,4 +16,8 @@ import org.springframework.stereotype.Service;
 public class JwtService {
 
     private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
 }
